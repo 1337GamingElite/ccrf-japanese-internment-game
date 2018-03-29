ce248594c9cc45ee68d9bcf0230d84ef057881d5
John M <1337gamingelite@gmail.com>
2018-03-28 18:53:33 -0600

    Framework for "E to interact"

---
+++ That Camp, My Life/Assets/Scripts/Stairs.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stairs : MonoBehaviour {
+
+	public int sceneIndex;
+	public GameObject pressE;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			pressE.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			pressE.SetActive(false);
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Interact") && pressE.active)
+		{
+			Debug.Log("Hi");
+		}
+	}
+
+}
